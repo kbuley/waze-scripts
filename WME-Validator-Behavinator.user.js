50a35f8322da8f7dad38538b09830c3f27915e8b
Kevin Buley <kevin@buley.org>
2016-04-12 23:38:14 -0400

    First run at forcing Validator in to a tab

---
+++ WME-Validator-Behavinator.user.js
@@ -0,0 +1,139 @@
+/* global Waze */
+// ==UserScript==
+// @name         WME Validator Behavinator
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Force Validator to live in a tab like everyone else
+// @author       Kevin Buley
+// @include      https://www.waze.com/editor/*
+// @include      https://www.waze.com/*/editor/*
+// @include      https://editor-beta.waze.com/*
+// @version      0.01
+// @grant        none
+// @copyright    2016 Kevin Buley
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    function debugLog(message) {
+        window.console.log("WME-ValidatorBehavinator:" + message);
+        return;
+    }
+    function getElementsByClassName(classname, node) {
+        if (!node) node = document.getElementsByTagName("body")[0];
+        var a = [];
+        var re = new RegExp('\\b' + classname + '\\b');
+        var els = node.getElementsByTagName("*");
+        for (var i = 0, j = els.length; i < j; i++)
+            if (re.test(els[i].className)) a.push(els[i]);
+        return a;
+    }
+    function getId(node) {
+        return document.getElementById(node);
+    }
+
+
+    function awaitLogin(e) {
+        if (e && e.user === null) {
+            return;
+        }
+
+        if (typeof Waze === 'undefined' || typeof Waze.loginManager === 'undefined') {
+            setTimeout(awaitLogin, 100);
+            return;
+        }
+
+        if (!Waze.loginManager.hasUser()) {
+            Waze.loginManager.events.register("login", null, awaitLogin);
+            Waze.loginManager.events.register("loginStatus", null, awaitLogin);
+            return;
+        }
+
+        if (getId('WazeMap') === null) {
+            setTimeout(awaitLogin, 100);
+            return;
+        }
+
+        var Validator = $($('a:contains("translate Validator")').filter(function(){ return $(this).children().length === 0;}).parent().parent().parent())[0];
+        if (typeof Validator === 'undefined') {
+            setTimeout(awaitLogin, 100);
+            return;
+        }
+        init();
+    }
+
+    function getPanelHandles(e) {
+        if (getId('user-info') === null) {
+            setTimeout(getPanelHandles, 100);
+        }
+
+        var userTabs = getId('user-info');
+
+        if (getElementsByClassName('nav-tabs',userTabs)[0] === undefined) {
+            setTimeout(getPanelHandles, 100);
+        }
+
+        var navTabs = getElementsByClassName('nav-tabs',userTabs)[0];
+
+        if (getElementsByClassName('tab-content', userTabs)[0] === undefined) {
+            setTimeout(getPanelHandles, 100);
+        }
+
+        var tabContent = getElementsByClassName('tab-content', userTabs)[0];
+
+        return {
+            'userTabs' : userTabs,
+            'navTabs' : navTabs,
+            'tabContent' : tabContent
+        };
+    }
+
+    function initTab() {
+        try {
+            // Get handles on the tabs and panels
+            var tabHandles = getPanelHandles();
+
+            // Create the tab
+            var myTab = document.createElement('li');
+            myTab.innerHTML = '<a id="sidepanel-behavinator-tab" href="#sidepanel-behavinator" data-toggle="tab" style="padding: 4px;">Validator</a>';
+
+            // Create the panel
+            var myTabPanel = document.createElement('section');
+            myTabPanel.id = "sidepanel-behavinator";
+            myTabPanel.className = "tab-pane";
+
+            // Create the tab content
+            var myTabContent = document.createElement('p');
+            myTabContent.style.padding = "padding: 8px 16px;";
+            myTabContent.id = "behavinatorTab";
+            WrapValidator(myTabContent);
+
+            // Attach the tab to the tab bar
+            tabHandles.navTabs.appendChild(myTab);
+            // Attach the content to the panel
+            myTabPanel.appendChild(myTabContent);
+            // Attach the panel to the sidebar
+            tabHandles.tabContent.appendChild(myTabPanel);
+        } catch (err) {
+            debugLog(err.message);
+        }
+    }
+
+    function WrapValidator(myTab) {
+        // Find out where validator is
+        var Validator = $($('a:contains("translate Validator")').filter(function(){ return $(this).children().length === 0;}).parent().parent().parent())[0];
+        if (Validator.parentElement.id === 'user-details') {
+            while (Validator.childNodes.length > 0) {
+                myTab.appendChild(Validator.childNodes[0]);
+            }
+        }
+        return;
+    }
+
+    function init() {
+        initTab();
+    }
+
+    debugLog("Starting up");
+    awaitLogin();
+})();
